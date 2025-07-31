d05153a06f2fa32710291b0aa16bc934ecba572c
Anton Pushkarev <a.pushkarev@kassir.ru>
2025-07-31 11:21:19 +0400

    feat: add custom script to app head in nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,13 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint', '@nuxt/scripts', '@nuxt/ui'],
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      script: [
+        {
+          innerHTML: 'var KkmServerAddIn = {};',
+        },
+      ],
+    },
+  },
 })
